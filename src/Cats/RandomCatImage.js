f92dcf2af98f856649359a70c1e0c1a546832086
SanzharDev <sanzhar.suleimenov@gmail.com>
2020-08-02 20:41:34 +0600

    My awesome commit for HW2 part1

--- src/Cats/RandomCatImage.js
+++ src/Cats/RandomCatImage.js
@@ -13,23 +13,32 @@ const Image = styled.img`
 // https://docs.thecatapi.com/
 
 function RandomCatImage(props) {
+  const [initalTimerValue, setInitialTimerValue] = React.useState(30);
   const [catImageUrl, setCatImageUrl] = React.useState(null);
+  const [counter, setCounter] = React.useState(initalTimerValue);
 
   React.useEffect(() => {
-    fetch("https://api.thecatapi.com/v1/images/search")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setCatImageUrl(data[0].url);
-      });
-  }, []);
+      const timer = counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
+      if (counter === initalTimerValue) {
+        fetch("https://api.thecatapi.com/v1/images/search")
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          setCatImageUrl(data[0].url);
+        });
+        setCounter(initalTimerValue);
+      }
+      if (counter === 0) setCounter(initalTimerValue);
+      return () => clearInterval(timer);
+  }, [counter]);
 
-  if (catImageUrl == null) return <div> Loading </div>;
+if (catImageUrl == null) return <div> Loading </div>;
 
   return (
     <MainContainer>
       <Image src={catImageUrl} />
+      <div>New cat image will appear in {counter} seconds</div>
     </MainContainer>
   );
 }
